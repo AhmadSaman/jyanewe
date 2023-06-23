918e39919c977c678d903214459f2b2cf323954c
Revan Sarbast Salih <revansarbast8@gmail.com>
2023-06-23 08:55:04 +0300

    add authantication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://otgivxdqnsvtehjctpdl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90Z2l2eGRxbnN2dGVoamN0cGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc0NjQzNDEsImV4cCI6MjAwMzA0MDM0MX0.5C_kgoHHXfUyLuOYrKBW8eyAfr3cJdDZHbPyx_XosvQ"
+);
 
 function App() {
   return <RouterProvider router={router} />;
